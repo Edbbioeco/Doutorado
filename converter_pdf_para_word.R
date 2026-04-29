6c827644a7483764fa70b8f42d2f1e8621d97ebd
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-29 11:22:07 -0300

    Criar docx vazio

--- converter_pdf_para_word.R
+++ converter_pdf_para_word.R
@@ -7,3 +7,9 @@ library(tesseract)
 pdf <- tesseract::ocr("C:/Users/LENOVO/OneDrive/Documentos/Projeto doutorado/Anteprojeto - Ericles Charles.pdf")
 
 pdf
+
+# Criar um .docx vazio ----
+
+doc <- officer::read_docx()
+
+doc
