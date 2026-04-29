9a96d08b26b7ad7869224a28939284a883d0cf90
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-29 11:06:04 -0300

    Comandos para criar o repositório

---
+++ criar_repositório.R
@@ -0,0 +1,15 @@
+# Pacotes ----
+
+library(usethis)
+
+# Iniciar o Git -----
+
+usethis::use_git()
+
+# Criar o repositório ----
+
+usethis::use_github()
+
+# Criar README -----
+
+usethis::use_readme_md()
