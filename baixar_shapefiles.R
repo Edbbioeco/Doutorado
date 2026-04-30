41e45bbf016a50a705f9ce0bb1ec9a68ab4c9f76
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-30 15:34:11 -0300

    Baixar, visualizar e exportar o shapefile da área do Brasil

--- baixar_shapefiles.R
+++ baixar_shapefiles.R
@@ -5,3 +5,22 @@ library(geobr)
 library(tidyverse)
 
 library(sf)
+
+# Shapefiles ----
+
+## Brasil ----
+
+### Baixar ----
+
+br <- geobr::read_country(year = 2019)
+
+### Visualizar ----
+
+br
+
+ggplot() +
+  geom_sf(data = br)
+
+### Exportar ----
+
+br |> sf::st_write("br.shp")
