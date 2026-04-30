db456ebe16fe327236429d45da32b2fd2f36645c
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-30 16:11:17 -0300

    Importar e visualizar os estados do CEP

--- mapa_cep.R
+++ mapa_cep.R
@@ -24,3 +24,16 @@ br
 
 ggplot() +
   geom_sf(data = br, color = "black")
+
+## Estados do CEP ----
+
+### Importar ----
+
+estados_cep <- sf::st_read("estados_cep.shp")
+
+### Visualizar ----
+
+ggplot() +
+  geom_sf(data = br, color = "black") +
+  geom_sf(data = estados_cep, color = "black", fill = "goldenrod")
+
