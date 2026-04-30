c955864e1240a359c69ebda18fb093b7f332cda6
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-30 15:23:52 -0300

    Importar e visualizar os dados dos estados do Brasil

--- mapa_cep.R
+++ mapa_cep.R
@@ -11,3 +11,18 @@ library(ggspatial)
 library(ggview)
 
 library(patchwork)
+
+# Dados ----
+
+## Estados do Brasil ----
+
+### Importar ----
+
+br <- geobr::read_state(year = 2019)
+
+### Visualizar ----
+
+br
+
+ggplot() +
+  geom_sf(data = br, color = "black")
